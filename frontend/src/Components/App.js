3350aa6040835f099dd918b602eec2a47a0db634
caozilu <caozilu@outlook.com>
2020-03-22 21:38:04 +0800

    build PlayerSingle component

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -40,14 +40,18 @@ class App extends React.Component {
     return (
       <div className="container-fluid">
         <div className="row">
-          <div className="col s12">Menu</div>
+          <nav>
+            <div className="nav-wrapper blue darken-1">
+              <a href="#" class="brand-logo">Soccer Management</a>
+            </div>
+          </nav>          
         </div>
         <div className="row">
           <div className="col s3">
             <PlayerList players={this.state.players} updateCurrentPlayer={this.state.updateCurrentPlayer} />
           </div>
           <div className="col s9">
-            <PlayerSingle />
+            <PlayerSingle player={this.state.currentPlayer} />
           </div>
         </div>
         <div className="row">
--- frontend/src/Components/Player/PlayerSingle.js
+++ frontend/src/Components/Player/PlayerSingle.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
-const PlayerSingle = () => {
+const PlayerSingle = (props) => {
     return (
-        <div>
-            PlayerSingle
+    <div className="row">
+        <div className="col s12 m7">
+          <div className="card">
+            <div className="card-image">
+              <img src="soccer_image.jpeg" alt="soccer image"/>
+              <span className="card-title">{props.player.firstName} {props.player.lastName}</span>
+            </div>
+            <div className="card-content">
+              <p>Phone: {props.player.phone} - Email: {props.player.email}</p>
+              <p>Strength: {props.player.strength} - Endurance: {props.player.endurance}</p>
+            </div>
+            <div className="card-action">
+              Team: {props.player.team}
+            </div>
+          </div>
         </div>
+    </div>
     );
 }
 
