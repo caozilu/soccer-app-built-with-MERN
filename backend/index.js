9bb5e3a726cd05063d1b65730657a92863a3fbae
caozilu <caozilu@outlook.com>
2020-03-22 15:45:19 +0800

    add POST endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyparser from 'body-parser';
+import routes from './routes/soccerRoutes';
 
 const app = express();
 const PORT = 3000;
@@ -15,6 +16,8 @@ mongoose.connect('mongodb://localhost/soccerDB', {
 //bodyParser setup
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
+
+routes(app);
 
 app.get('/', (req, res) => 
     res.send(`Our soccer application is running on port ${PORT}`)
