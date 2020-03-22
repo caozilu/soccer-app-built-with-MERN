42d0f8b82bca2b0d736421e0ca44722de1ba7d3b
caozilu <caozilu@outlook.com>
2020-03-22 16:02:42 +0800

    add GET all endpoint

--- backend/controllers/playerControllers.js
+++ backend/controllers/playerControllers.js
@@ -11,4 +11,13 @@ export const addNewPlayer = (req, res) => {
         }
         res.json(Player);
     })
-}
+};
+
+export const getPlayers = (req, res) => {
+    Player.find({}, (err, Player) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(Player);
+    })
+};
--- backend/routes/soccerRoutes.js
+++ backend/routes/soccerRoutes.js
@@ -1,9 +1,12 @@
-import { addNewPlayer } from '../controllers/playerControllers';
+import { addNewPlayer, getPlayers } from '../controllers/playerControllers';
 
 const routes = (app) => {
     app.route('/players')
+    //Get endpoint
+    .get(getPlayers)
+    
     //POST endpoint
-        .post(addNewPlayer);
+    .post(addNewPlayer);
 }
 
 export default routes;
