e8dd0f5b7a935030ddad221830646e73bf1ce719
caozilu <caozilu@outlook.com>
2020-03-22 21:05:02 +0800

    add state and axios

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,30 +1,63 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 import PlayerList from './Player/PlayerList';
 import PlayerSingle from './Player/PlayerSingle';
 import PlayerForm from './Player/PlayerForm';
 
-function App() {
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col s12">Menu</div>
-      </div>
-      <div className="row">
-        <div className="col s3">
-          <PlayerList />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: [],
+      currentPlayer: {}
+      }
+
+      this.updateCurrentPlayer = this.updateCurrentPlayer.bind(this);
+  }
+
+  componentDidMount(){
+    const url = 'http://localhost:4000/players';
+
+    axios.get(url)
+    .then((Response) => {
+      this.setState({
+        players: Response.data
+      })
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  updateCurrentPlayer(item){
+    this.setState({
+      currentPlayer: item
+    })
+  }
+
+  render() { 
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col s12">Menu</div>
         </div>
-        <div className="col s9">
-          <PlayerSingle />
+        <div className="row">
+          <div className="col s3">
+            <PlayerList players={this.state.players} updateCurrentPlayer={this.state.updateCurrentPlayer} />
+          </div>
+          <div className="col s9">
+            <PlayerSingle />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12">
+            <PlayerForm />
+          </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col s12">
-          <PlayerForm />
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
